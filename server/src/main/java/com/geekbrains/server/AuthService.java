1c982b7d259049647d0f18b8482758e5f750e0db
tina-95 <47135919+tina-95@users.noreply.github.com>
2021-01-26 20:36:25 +0300

    Add files via upload

---
+++ server/src/main/java/com/geekbrains/server/AuthService.java
@@ -0,0 +1,7 @@
+package com.geekbrains.server;
+
+public interface AuthService {
+    String getNicknameByLoginAndPassword(String login, String password);
+
+    boolean updateNick(String oldNick, String newNick);
+}
