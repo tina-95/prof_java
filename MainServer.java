82101723df24340fd843eb64aa0fccf5d3063d0b
tina-95 <47135919+tina-95@users.noreply.github.com>
2021-02-05 09:12:06 +0300

    Delete MainServer.java

--- MainServer.java
+++
@@ -1,9 +0,0 @@
-package com.geekbrains.server;
-
-import java.sql.SQLException;
-
-public class MainServer {
-    public static void main(String[] args) throws SQLException {
-        new Server();
-    }
-}
