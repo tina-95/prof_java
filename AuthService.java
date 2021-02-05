3ae00aa449f919db1885a1c2f666b1331e4368e3
tina-95 <47135919+tina-95@users.noreply.github.com>
2021-02-05 09:11:25 +0300

    Delete AuthService.java

--- AuthService.java
+++
@@ -1,9 +0,0 @@
-
-package com.geekbrains.server;
-
-
-public interface AuthService {
-    String getNicknameByLoginAndPassword(String login, String password);
-
-    boolean updateNick(String oldNick, String newNick);
-}
